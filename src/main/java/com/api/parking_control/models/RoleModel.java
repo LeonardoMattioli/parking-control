928a4c93910b25f4f574e8886496cac13572f5b4
LeonardoMattioli <leonardomattioli00@gmail.com>
2024-08-24 12:00:11 -0300

    create Role Model to implements roles in authentication

---
+++ src/main/java/com/api/parking_control/models/RoleModel.java
@@ -0,0 +1,42 @@
+package com.api.parking_control.models;
+
+import com.api.parking_control.enums.RoleName;
+import jakarta.persistence.*;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+@Entity
+@Table(name = "TB_ROLE")
+public class RoleModel implements Serializable, GrantedAuthority {
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false, unique = true)
+    private RoleName roleName;
+
+    @Override
+    public String getAuthority() {
+        return this.roleName.toString();
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public RoleName getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(RoleName roleName) {
+        this.roleName = roleName;
+    }
+}
